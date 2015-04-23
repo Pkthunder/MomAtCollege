540e7f27ccb385d33d05f3b281c78701bbb1e503
Dalton James <dalton_james@student.uml.edu>
2015-04-23 12:26:20 -0400

    making it stable

--- app/src/main/java/androiddev/jared/momatcollege/AlarmReceiver.java
+++ app/src/main/java/androiddev/jared/momatcollege/AlarmReceiver.java
@@ -10,26 +10,26 @@ import android.content.Intent;
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        setAlarms(context);
+        //setAlarms(context);
     }
 
-    public static void setAlarms(Context context){
-        //getAlarms();
-        //for( each alarm : alarms )
-
-        PendingIntent pi = createPendingIntent(context, alarm);
-    }
-
-    private static PendingIntent createPendingIntent(Context context, AlarmModel alarm){
-        Intent intent = new Intent(context, AlarmService.class);
-        intent.putExtra(ID, alarm.id);
-        intent.putExtra(NAME, alarm.name);
-        intent.putExtra(TIME_HOUR, alarm.timeHour);
-        intent.putExtra(TIME_MINUTE, alarm.timeMinute);
-        intent.putExtra(ALARM_INFO, alarm.alarmInfo);
-
-        return PendingIntent.getService(context, (int) alarm.id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-    }
+//    public static void setAlarms(Context context){
+//        //getAlarms();
+//        //for( each alarm : alarms )
+//
+//        PendingIntent pi = createPendingIntent(context, alarm);
+//    }
+//
+//    private static PendingIntent createPendingIntent(Context context, AlarmModel alarm){
+//        Intent intent = new Intent(context, AlarmService.class);
+//        intent.putExtra(ID, alarm.id);
+//        intent.putExtra(NAME, alarm.name);
+//        intent.putExtra(TIME_HOUR, alarm.timeHour);
+//        intent.putExtra(TIME_MINUTE, alarm.timeMinute);
+//        intent.putExtra(ALARM_INFO, alarm.alarmInfo);
+//
+//        return PendingIntent.getService(context, (int) alarm.id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//    }
 
 
     public void setAlarm(Context context){
